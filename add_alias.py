495b8de0e1965018ca31879a4a93ed08a526f93c
AxelusSS <mjugepa@yahoo.com>
2026-09-16 16:41:12 +0200

    feat: add /jump alias to /parkour

---
+++ add_alias.py
@@ -0,0 +1,10 @@
+﻿import sys
+
+yml_path = 'src/main/resources/plugin.yml'
+with open(yml_path, 'r', encoding='utf-8') as f:
+    yml = f.read()
+
+yml = yml.replace('  parkour:\n    description: Commencer le jump\n  team:', '  parkour:\n    description: Commencer le jump\n    aliases: [jump]\n  team:')
+
+with open(yml_path, 'w', encoding='utf-8') as f:
+    f.write(yml)
